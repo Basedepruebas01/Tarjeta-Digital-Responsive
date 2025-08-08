1934f99a4ea20b31e634f9dfe2fc907de84a52d6
basedepruebas01 <basedepruebas01@gmail.com>
2025-08-07 20:57:20 -0500

    Tarjeta Digital

---
+++ js/boton.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Espera a que se cargue la página y luego agrega la clase 'visible' al botón
+    var boton = document.querySelector(".boton");
+    boton.classList.add("visible");
+});
+
